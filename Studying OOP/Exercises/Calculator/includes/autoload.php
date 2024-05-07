27ff38e1b1a1c7893462ac45853c8ad9b35aee4f
DustyC <123617229+DustyCs@users.noreply.github.com>
2024-05-07 17:03:35 +0800

    Latest Updateeeeee

--- Studying OOP/Exercises/Calculator/includes/autoload.php
+++ Studying OOP/Exercises/Calculator/includes/autoload.php
@@ -1,8 +1,37 @@
 <?php
+    // OLD
+    // spl_autoload_register("AutoLoader");
+    // function AutoLoader($class_name){
+    //     $path = "classes/";
+    //     $extension = ".class.php";
+    //     $full_path = $path . $class_name . $extension;
+
+    //     if (!file_exists($full_path)){
+    //         return false;
+    //     } 
+
+    //     include_once $full_path;
+    // }
+
+    // New Auto Loader With URL For Safety/Efficiency? 
+
     spl_autoload_register("AutoLoader");
     function AutoLoader($class_name){
-        $path = "classes/";
-        $extension = ".php";
+        // Difference Start
+        $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+        if (strpos($url, 'includes') !== false){
+            $path = '../classes/';
+        }
+        else {
+            $path = 'classes/';
+        }
+
+        // $path = "classes/"; ------ GONE
+
+        // Difference End
+        
+        $extension = ".class.php";
         $full_path = $path . $class_name . $extension;
 
         if (!file_exists($full_path)){
--- Studying OOP/Exercises/Calculator/index.php
+++ Studying OOP/Exercises/Calculator/index.php
@@ -11,8 +11,17 @@ include 'includes/autoload.php'
     <title>Document</title>
 </head>
 <body>
-    <?php 
-        
-    ?>
+    <form action="calculator.includes.php" method="post">
+        <p>Calculator</p>
+        <input type="number" name="number1" placeholder="First Number">
+        <select name="operator">
+            <option value="add">Addition</option>
+            <option value="sub">Subtraction</option>
+            <option value="div">Division</option>
+            <option value="muk">Multiplication</option>
+        </select>
+        <input type="number" name="number2" placeholder="Second Number">
+        <button type="submit" name="submit">Calculate</button>
+    </form>
 </body>
 </html>
