51ee12e1792de081dbfb5874232776ef5a69063f
DustyC <123617229+DustyCs@users.noreply.github.com>
2024-05-26 13:55:40 +0800

    Update on Practice2

---
+++ Studying OOP/Exercises/With Database/Practice2/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="top-main">
+            <div class="nav-main">
+                <div class="site-title"></div>
+                <div class="nav-tabs"><a href="#" class="nav-button">HOME</a></div>
+                <div class="nav-tabs"><a href="#" class="nav-button">ABOUT</a></div>
+                <div class="nav-tabs"><a href="#" class="nav-button">MANIFESTO</a></div>
+                <div class="nav-tabs"><a href="#" class="nav-button">STATUS</a></div>
+            </div>
+            <div class="user-status">
+                <div class="signup-tab">SIGN UP</div>
+                <div class="login-tab">LOGIN</div>
+            </div>
+        </div>
+        <div class="middle-main">
+            <div class="short-text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum vero quisquam saepe magni quisquam! Iusto, aut in?
+            </div>
+            <div class="big-text">
+                Not really a proper site to care
+            </div>
+        </div>
+        <div class="bottom-main">
+            <div class="signup">
+                <form action="" method="post">
+                    <h5>SIGN UP</h5>
+                    <p id="signup-text">
+                        Don't have an account yet? Sign up here!
+                    </p>
+                    <input type="text" name="username">
+                    <input type="text" name="password">
+                    <input type="text" name="rep-password">
+                    <input type="text" name="email">
+                    <input type="submit" value="SIGN UP">
+                </form>
+            </div>
+            <div class="login">
+                <form action="" method="post"></form>
+                <h5>LOGIN</h3>
+                <p id="login-text">
+                    
+                </p>
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+                <input type="submit" value="LOGIN">
+            </div>
+        </div>
+        <footer></footer>
+    </div>
+</body>
+</html>
