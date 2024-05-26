deaadf6dea8356eb195a4422d887d8d9e01ca663
DustyC <123617229+DustyCs@users.noreply.github.com>
2024-05-26 16:48:41 +0800

    Update on Practice2

--- Studying OOP/Exercises/With Database/Practice2/index.php
+++ Studying OOP/Exercises/With Database/Practice2/index.php
@@ -4,56 +4,63 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="static/master.css">
 </head>
 <body>
     <div class="container">
         <div class="top-main">
             <div class="nav-main">
-                <div class="site-title"></div>
+                <div class="nav-tabs site-title">SOMETHING THINK</div>
                 <div class="nav-tabs"><a href="#" class="nav-button">HOME</a></div>
                 <div class="nav-tabs"><a href="#" class="nav-button">ABOUT</a></div>
                 <div class="nav-tabs"><a href="#" class="nav-button">MANIFESTO</a></div>
                 <div class="nav-tabs"><a href="#" class="nav-button">STATUS</a></div>
             </div>
             <div class="user-status">
-                <div class="signup-tab">SIGN UP</div>
-                <div class="login-tab">LOGIN</div>
+                <div class="status-tab signup-tab">SIGN UP</div>
+                <div class="status-tab login-tab">LOGIN</div>
             </div>
         </div>
         <div class="middle-main">
-            <div class="short-text">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum vero quisquam saepe magni quisquam! Iusto, aut in?
-            </div>
-            <div class="big-text">
-                Not really a proper site to care
-            </div>
+            <div class="middle-container">
+                <div class="short-text">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Ipsum vero quisquam saepe magni quisquam! Iusto, aut in?
+                </div>
+                <div class="big-text">
+                    Not really a proper site to care
+                </div>
+            </div>     
         </div>
         <div class="bottom-main">
             <div class="signup">
-                <form action="" method="post">
+                <form action="" method="post" id="signup-form">
                     <h5>SIGN UP</h5>
                     <p id="signup-text">
                         Don't have an account yet? Sign up here!
                     </p>
-                    <input type="text" name="username">
-                    <input type="text" name="password">
-                    <input type="text" name="rep-password">
-                    <input type="text" name="email">
+                    <input type="text" name="username" placeholder="Username">
+                    <input type="text" name="password" placeholder="Password">
+                    <input type="text" name="rep-password" placeholder="Repeat Password">
+                    <input type="text" name="email" placeholder="Email">
                     <input type="submit" value="SIGN UP">
                 </form>
             </div>
             <div class="login">
-                <form action="" method="post"></form>
+                <form action="" method="post" id="login-form">
                 <h5>LOGIN</h3>
                 <p id="login-text">
-                    
+                    Done with the sign up? Login here!
                 </p>
-                <input type="text" name="" id="">
-                <input type="text" name="" id="">
+                <input type="text" name="username" placeholder="Username">
+                <input type="text" name="password" placeholder="Password">
                 <input type="submit" value="LOGIN">
+                </form>
             </div>
         </div>
-        <footer></footer>
+        <footer>
+            
+        </footer>
     </div>
 </body>
 </html>
