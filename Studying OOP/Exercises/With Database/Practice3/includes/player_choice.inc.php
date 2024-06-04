e97ac5bfbbcff5e3ac1ae9946b68da688ee664d1
DustyC <123617229+DustyCs@users.noreply.github.com>
2024-06-04 17:59:31 +0800

    Created and Update Practice3

---
+++ Studying OOP/Exercises/With Database/Practice3/includes/player_choice.inc.php
@@ -0,0 +1,16 @@
+<?php 
+
+if(isset($_POST["submit"])){
+    // ECHO "PASSED";
+
+    // Grab
+
+
+    // Istantiate
+
+    // Error Handling
+
+    // Header
+
+    header("location: ../index.php?error=none");
+}
---
+++ Studying OOP/Exercises/With Database/Practice3/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="statics/css/style.css">
+</head>
+<body>
+    <div class="navigation-bar">
+        <div class="main-nav">
+
+        </div>
+    </div>
+    <div class="main-container">
+        
+        <div class="main_player">
+            <form action="includes/" method="post" class="player_input">
+                <input type="radio" name="player" value="Rock" id="p_rock">
+                <label for="p_rock"><img src="statics/imgs/rock.jpg" alt="Rock" class="game_choice"></label>
+                <input type="radio" name="player" value="Paper" id="p_paper">
+                <label for="p_paper"><img src="statics/imgs/paper.jpg" alt="Paper" class="game_choice"></label>
+                <input type="radio" name="player" value="Scissors" id="p_scissors">
+                <label for="p_scissors"><img src="statics/imgs/scissors.jpg" alt="Scissors" class="game_choice"></label>
+            </form>
+        </div>
+        <div class="current_scores">
+            <div class="score_board">
+                <div class="board_title">
+                    <h4>Current Scores</h4>
+                </div>
+                <div class="board_scores">
+                    <div class="player_score">
+                        <div class="enemy_title">
+
+                        </div>
+                        <div class="player_score">
+
+                        </div>
+                    </div>
+                    <div class="enemy_score">
+                        <div class="enemy_title">
+
+                        </div>
+                        <div class="enemy_score">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main_enemy">
+            <form form action="includes/" method="post" class="enemy_input">
+                <input type="radio" name="enemy" value="Rock" id="e_rock">
+                <label for="e_rock"><img src="statics/imgs/rock.jpg" alt="Rock" class="game_choice"></label>
+                <input type="radio" name="enemy" value="Paper" id="e_paper">
+                <label for="e_paper"><img src="statics/imgs/paper.jpg" alt="Paper" class="game_choice"></label>
+                <input type="radio" name="enemy" value="Scissors" id="e_scissors">
+                <label for="e_scissors"><img src="statics/imgs/scissors.jpg" alt="Scissors" class="game_choice"></label>
+            </form> 
+        </div>
+    </div>
+   
+</body>
+</html>
