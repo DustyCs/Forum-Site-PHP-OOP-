f336cbe75c38fc7cb3effc2bbb5b749faaa7547d
DustyC <123617229+DustyCs@users.noreply.github.com>
2024-06-13 22:15:21 +0800

    Practice3 now can't play without selecting

--- Studying OOP/Exercises/With Database/Practice3/index.php
+++ Studying OOP/Exercises/With Database/Practice3/index.php
@@ -8,8 +8,10 @@
     <link rel="stylesheet" href="statics/css/result_popup.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="statics/js/jquery-3.7.1.min.js"></script>
-    <script src="statics/js//main_rps_func.js"></script>
+    <script src="statics/js/util_func.js"></script>
+    <script src="statics/js/main_rps_func.js"></script>
     <script src="statics/js/rps_js.js"></script>
+
 </head>
 <body>
     <div class="navigation-bar">
--- Studying OOP/Exercises/With Database/Practice3/statics/js/rps_js.js
+++ Studying OOP/Exercises/With Database/Practice3/statics/js/rps_js.js
@@ -1,67 +1,72 @@
 // Main
 
 $(document).ready(function() {  
-    $.getScript('main_rps_func.js');
- 
     let game_total = 0;
     let playerScore = 0;
-    let enemyScore = 0;   
-    $("#play").on('click', function(){
-        switch(Math.floor(Math.random() * 3)){
-            case 0:
-                $('#e_rock').prop('checked', true);
-                break;
-            case 1:
-                $('#e_paper').prop('checked', true);
-                break;
-            case 2:
-                $('#e_scissors').prop('checked', true);
-                break;
-            
-        }
-
-        let enemy_choice = $('input[name=enemy]:checked', '#enemy_form').val()
-        let player_choice = $('input[name=player]:checked', '#player_form').val();
-        let gameResult = rpsFun($('input[name=player]:checked', '#player_form').val(), $('input[name=enemy]:checked', '#enemy_form').val());
-        let result;
+    let enemyScore = 0; 
     
-
-        switch(gameResult){
-            case 0:
-                enemyScore = enemyScore + 1;
-                result = "Lose";
-                $('#result_popup_msg').text(result);
-                break;
-            case 1:
-                playerScore = playerScore + 1;
-                result = "Win";
-                $('#result_popup_msg').text(result);
-                break;
-            case 2:
-                playerScore = playerScore; 
-                result = "Tie";
-                $('#result_popup_msg').text(result);
-                break;
+    $("#play").on('click', function(){
+        if(!$('input[name=player]:checked', '#player_form').val()){
+            console.log('false')
         }
         
-        game_total = game_total + 1;
-        $("#player_score").load("includes/player_choice.inc.php",
-            {
-                 p_score: playerScore,
-                 game_result: result,
-                 playerChoice: player_choice,
-                 enemyChoice: enemy_choice,
-                 player_clicked: true
+        else{
+            switch(Math.floor(Math.random() * 3)){
+                case 0:
+                    $('#e_rock').prop('checked', true);
+                    break;
+                case 1:
+                    $('#e_paper').prop('checked', true);
+                    break;
+                case 2:
+                    $('#e_scissors').prop('checked', true);
+                    break;
+                
             }
-        );
 
-        $(".result_popup").show('fast');
-        setTimeout(function () {
-            $('.result_popup').fadeOut('slow');
-        }, 1500);
+            let enemy_choice = $('input[name=enemy]:checked', '#enemy_form').val()
+            let player_choice = $('input[name=player]:checked', '#player_form').val();
+            let gameResult = rpsFun($('input[name=player]:checked', '#player_form').val(), $('input[name=enemy]:checked', '#enemy_form').val());
+            let result;
         
-        console.log($('input[name=player]:checked', '#player_form').val());
-        console.log($('input[name=enemy]:checked', '#enemy_form').val());
+
+            switch(gameResult){
+                case 0:
+                    enemyScore = enemyScore + 1;
+                    result = "Lose";
+                    $('#result_popup_msg').text(result); // can also use .html instead of .text
+                    break;
+                case 1:
+                    playerScore = playerScore + 1;
+                    result = "Win";
+                    $('#result_popup_msg').text(result);
+                    break;
+                case 2:
+                    playerScore = playerScore; 
+                    result = "Tie";
+                    $('#result_popup_msg').text(result);
+                    break;
+            }
+            
+            game_total = game_total + 1;
+            $("#player_score").load("includes/player_choice.inc.php",
+                {
+                    p_score: playerScore,
+                    game_result: result,
+                    playerChoice: player_choice,
+                    enemyChoice: enemy_choice,
+                    player_clicked: true
+                }
+            );
+
+            $(".result_popup").show('fast');
+            setTimeout(function () {
+                $('.result_popup').fadeOut('fast');
+            }, 1500);
+            
+            console.log($('input[name=player]:checked', '#player_form').val());
+            console.log($('input[name=enemy]:checked', '#enemy_form').val());
+        }
     }
 );
 
