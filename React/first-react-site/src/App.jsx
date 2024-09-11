c38a357f60faf2c0686a89c5e974c6daad11ca20
DustyC <123617229+DustyCs@users.noreply.github.com>
2024-09-11 22:00:32 +0800

    Removed old react files after being a dum dum and install unnecessary dependencies just for tailwind and it turns out am suppose to install tailwind for vite not react

---
+++ React/first-react-site/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Header.jsx'
+import List from './List.jsx'
+
+function App() {
+
+  return (
+    <>
+     <Header />
+     <List />
+     
+    </>
+  )
+}
+
+export default App
---
+++ React/first-react-site/src/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return(
+        <div>
+            <h1>Header</h1>
+            <div>
+                <ul className="p-5 flex">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">About</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Header
